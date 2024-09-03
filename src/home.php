2f55df9ab4caaf7f719c859b9cfb60667e5c7160
irisdj <104412524+irisdj@users.noreply.github.com>
2024-09-03 17:04:01 +0800

    Add files via upload

---
+++ src/home.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Allocally PH | Everyday Local</title>
+    <link rel="stylesheet" href="main.css">
+    <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body class="bg-body-tertiary">
+
+  <nav class="navbar navbar-expand-sm bg-primary shadow-sm fixed-top">
+
+    <div class="container">
+
+      <a class="navbar-brand" href="#">
+        <img src="../img/companylogoy.png"alt="Allocally" width="120" height="50">
+      </a>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+
+        <ul class="navbar-nav ms-auto">
+
+          <li class="nav-item">
+            <a class="nav-link active text-secondary" aria-current="page" href="#">Home</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link text-secondary" href="#">About</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link text-secondary" href="#products">Shop</a>
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <main>
+    <div class="container-fluid bg-secondary pt-5 pb-1">
+      <p class="text-center fw-regular text-primary mt-5">Celebrating Local Women Artisans</p>
+    </div>
+    
+    <section id="carouselExample" class="carousel slide">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active c-item">
+          <img src="../img/habicarousel.png" class="d-block w-100 c-img" alt="...">
+          <div class="carousel-caption top-0 mt-4">
+            <p class="fs-4 mt-5 fw-light">Handmade bags, clutches, and wallets</p>
+            <h1 class="display-1 fw-bolder mb-5">Habi</h1>
+            <button class="btn btn-primary px-4 py-2 fs-5 mt-2">Shop</button>
+          </div>
+        </div>
+        <div class="carousel-item c-item">
+          <img src="../img/layagcarousel.png" class="d-block w-100 c-img" alt="...">
+          <div class="carousel-caption top-0 mt-4">
+            <p class="fs-4 mt-5 fw-light">Local heels. In the works.</p>
+            <h1 class="display-1 fw-bolder mb-5">Layag</h1>
+            <button class="btn btn-primary px-4 py-2 fs-5 mt-2">Shop</button>
+          </div>
+        </div>
+        <div class="carousel-item c-item">
+          <img src="../img/modacarousel.png" class="d-block w-100 c-img" alt="...">
+          <div class="carousel-caption top-0 mt-4">
+            <p class="fs-4 mt-5 fw-light">Local clothing line. Coming soon.</p>
+            <h1 class="display-1 fw-bolder mb-5">Moda</h1>
+            <button class="btn btn-primary px-4 py-2 fs-5 mt-2">Shop</button>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </section>
+
+    <section id="products" class="pt-md-5">
+      <h2 class="text-center my-5 text-primary">All Products</h2>
+      <div class="container">
+        <div class="row mb-4">
+
+          <div class="col-lg">
+            <div class="card border-0">
+              <img src="../img/microbag.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="lead card-text text-center">Habi Micro Bag</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg">
+            <div class="card border-0">
+              <img src="../img/microbag.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="lead card-text text-center">Habi Micro Bag</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg">
+            <div class="card border-0">
+              <img src="../img/microbag.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="lead card-text text-center">Habi Micro Bag</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg">
+            <div class="card border-0">
+              <img src="../img/microbag.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="lead card-text text-center">Habi Micro Bag</p>
+              </div>
+            </div>
+          </div>
+  
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg">
+            <div class="card border-0">
+              <img src="../img/microbag.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="lead card-text text-center">Habi Micro Bag</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg">
+            <div class="card border-0">
+              <img src="../img/microbag.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="lead card-text text-center">Habi Micro Bag</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg">
+            <div class="card border-0">
+              <img src="../img/microbag.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="lead card-text text-center">Habi Micro Bag</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg">
+            <div class="card border-0">
+              <img src="../img/microbag.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <p class="lead card-text text-center">Habi Micro Bag</p>
+              </div>
+            </div>
+          </div>
+          
+  
+        </div>
+      </div>
+      
+    </section>
+
+    <section id="faqs" class="py-md-5">
+      <h2 class="text-center my-5 text-primary">Frequently Asked Questions</h2>
+      <div class="class">
+        <div class="accordion w-75 mx-auto" id="accordionExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+              Do you accept orders for giveaways / souvenirs?
+              </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                <strong>Yes</strong>, we do accept for any life events such as (but not limited to) birthdays, weddings, and other celebrations.
+              </div>
+            </div>
+          </div>
+
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              What Habi piece can I order?
+              </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                We recommend our <strong>Habi Large Pocket & Habi Coin Purse</strong> but you can also choose any Habi piece.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <footer class="bg-secondary mt-5">
+    <div class="container text-primary py-5">
+      <div class="row">
+        <div class="col-7">
+          <h3>Never miss an update!</h3>
+          <p>Sign up to our newsletter for exclusive alerts on sales, special promos, new drops, and more!</p>
+          <form action="">
+          <div class="mb-3 w-50">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="email@example.com" aria-describedby="emailHelp">
+          </div>
+          </form>
+        </div>
+        <div class="col-3">
+          <ul class="list-unstyled">
+            <li><a href="#" class="link-primary text-uppercase">All Products</a></li>
+            <li><a href="#" class="link-primary text-uppercase">New</a></li>
+            <li><a href="#" class="link-primary text-uppercase">Sale</a></li>
+          </ul>
+        </div>
+
+        <div class="col">
+          <ul class="list-unstyled">
+            <li><a href="#" class="link-primary text-uppercase">Collections</a></li>
+            <li><a href="#" class="link-primary text-uppercase">FAQs</a></li>
+            <li><a href="#" class="link-primary text-uppercase">About Us</a></li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
